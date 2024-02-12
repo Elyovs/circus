24f6e4c2977a055b363be896893ceac99d14cba7
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-02-12 17:29:45 +0800

    Add all changes from lecture

---
+++ src/main/java/circus/animal/Animal.java
@@ -0,0 +1,15 @@
+package circus.animal;
+
+import circus.Asset;
+
+public abstract class Animal implements Asset {
+
+    public abstract String speak();
+//    {
+//        return null;
+//    }
+
+//    public int getValue() {
+//        return 10;
+//    }
+}
